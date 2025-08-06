28b424d89f3036c328af68a27c99f761dc5f1973
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:03:39 +0700

    fix: Gradle scripts🫸🌀✏️📗🐧🐳

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,34 @@
-import sun.jvmstat.monitor.MonitoredVmUtil.mainClass
-
-val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
 val ktorm_version: String by project
 val postgresql_driver_version: String by project
 
 plugins {
-    application
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
 }
 
 group = "id.my.hendisantika"
 version = "0.0.1"
-application {
-    mainClass.set("id.my.hendisantika.ktorktormpostgresql.KtorKtormPostgresqlApplication")
-}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     implementation("org.ktorm:ktorm-core:$ktorm_version")
     implementation("org.ktorm:ktorm-support-postgresql:$ktorm_version")
     implementation("org.postgresql:postgresql:$postgresql_driver_version")
 
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
