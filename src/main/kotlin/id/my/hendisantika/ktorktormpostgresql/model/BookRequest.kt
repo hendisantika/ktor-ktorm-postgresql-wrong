9e2303391a7c23a9deb6f568452fa5fd2964dc1e
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:19:30 +0700

    feat: Add BookRequest🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/ktorktormpostgresql/model/BookRequest.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.ktorktormpostgresql.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ktor-ktorm-postgresql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/08/25
+ * Time: 10.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Serializable
+data class BookRequest(
+    val name: String
+)
