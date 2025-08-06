c67a47a0163ed588f66e311cf0103adc13a56323
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:20:48 +0700

    feat: Add createBook in BookService🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/ktorktormpostgresql/service/BookService.kt
+++ src/main/kotlin/id/my/hendisantika/ktorktormpostgresql/service/BookService.kt
@@ -1,6 +1,11 @@
 package id.my.hendisantika.ktorktormpostgresql.service
 
+import id.my.hendisantika.ktorktormpostgresql.model.Book
+import id.my.hendisantika.ktorktormpostgresql.model.BookRequest
+import id.my.hendisantika.ktorktormpostgresql.model.Books
 import org.ktorm.database.Database
+import org.ktorm.entity.add
+import org.ktorm.entity.sequenceOf
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,16 @@ class BookService {
         user = "postgres",
         password = "postgres"
     )
+
+    fun createBook(bookRequest: BookRequest): Boolean {
+        val newBook = Book {
+            name = bookRequest.name
+        }
+
+        val affectedRecordsNumber =
+            database.sequenceOf(Books)
+                .add(newBook)
+
+        return affectedRecordsNumber == 1
+    }
 }
