c80472e2b84741d50320443b97c92fa3c86bad55
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 10:18:02 +0700

    feat: Add Book Object🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/ktorktormpostgresql/model/Book.kt
@@ -0,0 +1,22 @@
+package id.my.hendisantika.ktorktormpostgresql.model
+
+import org.ktorm.schema.Table
+import org.ktorm.schema.long
+import org.ktorm.schema.varchar
+import java.awt.print.Book
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ktor-ktorm-postgresql
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/08/25
+ * Time: 10.17
+ * To change this template use File | Settings | File Templates.
+ */
+object Books : Table<Book>("book") {
+    val id = long("id").primaryKey().bindTo(Book::id)
+    val name = varchar("name").bindTo(Book::name)
+}
