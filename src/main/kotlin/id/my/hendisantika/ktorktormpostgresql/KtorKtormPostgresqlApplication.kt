fd90e54174f99d475415f8bc3bdcfafc84d336c4
hendisantika <hendisantika@yahoo.co.id>
2025-08-06 09:09:31 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/ktorktormpostgresql/KtorKtormPostgresqlApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.ktorktormpostgresql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtorKtormPostgresqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtorKtormPostgresqlApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/ktorktormpostgresql/KtorKtormPostgresqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ktorktormpostgresql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtorKtormPostgresqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
